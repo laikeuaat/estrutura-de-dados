ed06aef8097248e4eb0630d721128bc700a5c882
Camila Fernanda de Carvalho <camilafcarvalho07@gmail.com>
2024-03-23 16:10:24 -0300

    Adicionado o exercicio proposto na aula

---
+++ PontoCartesiano.java
@@ -0,0 +1,44 @@
+import java.lang.Math;
+
+public class PontoCartesiano {
+    private double pontoX;
+    private double pontoY;
+
+    public PontoCartesiano(double pontoX, double pontoY) {
+        this.pontoX = pontoX;
+        this.pontoY = pontoY;
+    }
+
+    public double getPontoX() {
+        return pontoX;
+    }
+
+    public double getPontoY() {
+        return pontoY;
+    }
+
+    public void setPontoX(double x) {
+        pontoX = x;
+    }
+
+    public void setPontoY(double y) {
+        pontoY = y;
+    }
+
+    public double calcular_distancia(PontoCartesiano p) {
+        double cateto_a = pontoX - p.pontoX;
+        if (cateto_a < 0) {
+            Math.abs(cateto_a);
+
+        }
+        System.out.println(cateto_a);
+        double cateto_b = pontoY - p.pontoY;
+        if (cateto_b < 0) {
+            Math.abs(cateto_b);
+
+        }
+        System.out.println(cateto_b);
+        double distancia = Math.sqrt(Math.pow(cateto_a, 2) + Math.pow(cateto_b, 2));
+        return distancia;
+    }
+}
---
+++ main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        PontoCartesiano a = new PontoCartesiano(10, 10);
+        PontoCartesiano b = new PontoCartesiano(1, 1);
+        System.out.println(a.calcular_distancia(b));
+    }
+
+}
